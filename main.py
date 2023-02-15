29f57cc57333c6e2a4b5777b4c8dcfa31440fe49
asoshina <alenasoshina@gmail.com>
2023-02-15 21:40:00 +0200

    multi-user bot

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import telebot
 API_TOKEN = '6267872736:AAFif1GKVPKThRpBTqNqnqvX8RObPwKudN4'
 shop_bot = telebot.TeleBot(API_TOKEN)
 
-# telebot.types.ReplyKeyboardMarkup
-# telebot.types.InlineKeyboardMarkup
-
 commands = ['Add to list', 'Get list', 'Remove from list']
 keyboard = telebot.types.ReplyKeyboardMarkup()
 keyboard.row(*commands)
 
-
-shopping_list = []
-current_operation = None
-last_message_id = None
+# {<chat_id>: [shopping_list, current_operation, last_message_id]}
+user_data = {}
 
 
 @shop_bot.message_handler(commands=['start'])
@@ -23,38 +18,39 @@ def start(message):
 
 @shop_bot.message_handler(content_types=['text'])
 def command(message):
-    global current_operation
-    global shopping_list
-    global last_message_id
+    if message.chat.id not in user_data:
+        user_data[message.chat.id] = [[], None, None]
     if message.text == 'Add to list':
-        current_operation = 'add'
+        user_data[message.chat.id][1] = 'add'
         shop_bot.reply_to(message, 'Add items to the list')
-    elif message.text == 'Get from list':
-        current_operation = 'get'
-        shop_bot.reply_to(message, ', '.join(shopping_list) if shopping_list else 'Your list is empty')
+    elif message.text == 'Get list':
+        user_data[message.chat.id][1] = 'get'
+        shop_bot.reply_to(message, ', '.join(user_data[message.chat.id][0]) if user_data[message.chat.id][0] else 'Your list is empty')
     elif message.text == 'Remove from list':
-        current_operation = 'rm'
+        user_data[message.chat.id][1] = 'rm'
         inline_kb = telebot.types.InlineKeyboardMarkup()
-        for elem in shopping_list:
+        inline_kb.row_width = 2
+        for elem in user_data[message.chat.id][0]:
             inline_kb.add(telebot.types.InlineKeyboardButton(elem, callback_data=elem))
         res = shop_bot.reply_to(message, 'Now you can delete item', reply_markup=inline_kb)
-        last_message_id = res.message_id
+        user_data[message.chat.id][2] = res.message_id
     else:
-        if current_operation == 'add':
-            shopping_list.append(message.text)
+        if user_data[message.chat.id][1] == 'add':
+            user_data[message.chat.id][0].append(message.text)
             shop_bot.reply_to(message, f'successfully add {message.text}')
 
 
-@shop_bot.callback_query_handler(func=lambda x:True)
+@shop_bot.callback_query_handler(func=lambda x: True)
 def callback_handler(call):
-    global shopping_list
-    if call.data in shopping_list:
-        shopping_list.remove(call.data)
+    if call.data in user_data[call.message.chat.id][0]:
+        user_data[call.message.chat.id][0].remove(call.data)
 
     inline_kb = telebot.types.InlineKeyboardMarkup()
-    for elem in shopping_list:
+    inline_kb.row_width = 2
+
+    for elem in user_data[call.message.chat.id][0]:
         inline_kb.add(telebot.types.InlineKeyboardButton(elem, callback_data=elem))
-    shop_bot.edit_message_reply_markup(call.message.chat.id, last_message_id, reply_markup=inline_kb)
+    shop_bot.edit_message_reply_markup(call.message.chat.id, user_data[call.message.chat.id][2], reply_markup=inline_kb)
 
 
 shop_bot.polling()
